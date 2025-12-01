9bd11a62012a2f57b3bc3e172a58ca5ceadce583
holetinnghia <holetinnghia@icloud.com>
2025-12-01 21:43:12 +0700

    fix(util): ping to render

---
+++ utils/render_ping_util.py
@@ -0,0 +1,19 @@
+import asyncio
+import aiohttp
+import logging
+
+async def self_ping_task():
+    url = "https://anh-nghai-bot.onrender.com"
+    logging.info(f"Đã kích hoạt chế độ tự ping mỗi 5 phút vào: {url}")
+    while True:
+        await asyncio.sleep(300)  # 5 phút
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    if resp.status == 200:
+                        logging.info("Tự ping thành công (Bot vẫn sống)")
+                    else:
+                        logging.warning(f"⚠️ Tự ping thất bại: {resp.status}")
+        except aiohttp.ClientError as e:
+            logging.error(f"❌ Lỗi tự ping: {e}")
+            await asyncio.sleep(60)
